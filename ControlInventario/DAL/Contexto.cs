32627c4040e96efa8c5a5c1a2c27c91d9fd59398
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-07-13 19:59:16 -0400

    Contexto terminado

---
+++ ControlInventario/DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using ControlInventario.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlInventario.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Categorias> Categoria { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<EntradaProductos> EntradaProductos { get; set; }
+        public DbSet<Facturas> Facturas { get; set; }
+        public DbSet<CuadresDeCaja> CuadresDeCajas { get; set; }
+
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
